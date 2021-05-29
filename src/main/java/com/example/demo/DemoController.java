f331d714c46a6b39bb519bc3086829ec9a4448d9
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-05-29 11:38:33 +0900

    add

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/demo1")
+    public String demo(HttpSession session){
+        return getString(session);
+    }
+
+    @GetMapping("/demo2")
+    public String demo2(HttpSession session){
+        return getString(session);
+    }
+
+
+    private String getString(HttpSession session) {
+        var value = (Integer)session.getAttribute("a");
+        logger.debug("value={}", value);
+
+        if(null == value){
+            session.setAttribute("a", 0);
+        }else{
+            value++;
+            session.setAttribute("a",value);
+        }
+
+        return  String.format("Hello %s", value);
+    }
+}
---
+++ src/main/java/com/example/demo/WebAppConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+//@Configuration
+//@Import({MySessionListener.class})
+public class WebAppConfig {
+}
