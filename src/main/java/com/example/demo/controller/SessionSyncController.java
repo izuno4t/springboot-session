0b64278dc2bf8d1ac77ca56c1b9cca26dca65cff
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-07-24 21:16:32 +0900

    up

--- src/main/java/com/example/demo/controller/SessionSyncController.java
+++ src/main/java/com/example/demo/controller/SessionSyncController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sync")
 public class SessionSyncController {
 
-    @RequestMapping(value = "/{param}")
+    private static final Logger logger = LoggerFactory.getLogger(SessionSyncController.class);
+
+    @GetMapping("/index")
+    public String index(HttpSession session){
+        session.setAttribute("foo", null);
+        session.setAttribute("bar", null);
+
+        logger.info("session foo={}", session.getAttribute("foo"));
+        logger.info("session bar={}", session.getAttribute("bar"));
+        return "clear session:" + session.getId();
+    }
+
+    @GetMapping(value = "/{param}")
     public String sayHello(@PathVariable String param, HttpSession session) {
-        System.out.println("param=" + param);
+        logger.info("param=" + param);
 
         Object id = session.getId();
         if (id == null) {
-            System.out.println("session not exist " + id);
+            logger.info("session not exist " + id);
             //session.setAttribute("p", p);
         } else {
-            System.out.println("session existed ，id=" + id);
+            logger.info("session existed ，id=" + id);
             if (param.equals("sleep")) {
                 try {
                     Thread.sleep(15000);
@@ -27,9 +42,15 @@ public class SessionSyncController {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-                System.out.println("wake up!");
+                logger.info("wake up!");
+                session.setAttribute("foo", "wake up");
+            }else{
+                session.setAttribute("bar", "foo bar foo bar");
             }
         }
+        logger.info("session foo={}", session.getAttribute("foo"));
+        logger.info("session bar={}", session.getAttribute("bar"));
+
         return "Hello,World! " + param;
     }
 }
