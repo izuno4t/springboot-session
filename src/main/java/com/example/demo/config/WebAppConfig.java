df709e83509a5e81a7132976e2b11fd73651f7f5
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-07-24 21:04:54 +0900

    up

--- src/main/java/com/example/demo/config/WebAppConfig.java
+++ src/main/java/com/example/demo/config/WebAppConfig.java
@@ -1,13 +1,16 @@
 package com.example.demo.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@Configuration
 @EnableRedisHttpSession
 public class WebAppConfig {
-
-
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory();
+    }
 }
--- src/main/java/com/example/demo/controller/SyncSessionRequest.java
+++ src/main/java/com/example/demo/controller/SyncSessionRequest.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sync")
 public class SyncSessionRequest {
 
-    @RequestMapping(value = "/{param}")
+    @GetMapping("/index")
+    public String index(HttpSession session){
+        session.setAttribute("p", null);
+        session.setAttribute("q", null);
+        System.out.println("p="+session.getAttribute("p"));
+        System.out.println("q="+session.getAttribute("q"));
+        return "index";
+    }
+
+
+    @GetMapping(value = "/{param}")
     public String sayHello(@PathVariable String param,  HttpSession session) {
         System.out.println("param=" + param);
 
         Object id = session.getId();
         if (id == null) {
             System.out.println("session not exist " + id);
-            //session.setAttribute("p", p);
         } else {
             System.out.println("session existed ，id=" + id);
             if (param.equals("sleep")) {
@@ -28,8 +38,14 @@ public class SyncSessionRequest {
                     e.printStackTrace();
                 }
                 System.out.println("wake up!");
+                session.setAttribute("q", "q");
+            }else{
+                session.setAttribute("p", "p");
             }
         }
+        System.out.println("p="+session.getAttribute("p"));
+        System.out.println("q="+session.getAttribute("q"));
+
         return "Hello,World! " + param;
     }
 }
