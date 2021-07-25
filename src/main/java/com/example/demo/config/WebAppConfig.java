6d0a2ac5267c9d12e12f7bba2315b2e57fe60755
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-07-25 09:51:13 +0900

    Redisをつかわなければ同期化される。。。

--- src/main/java/com/example/demo/config/WebAppConfig.java
+++ src/main/java/com/example/demo/config/WebAppConfig.java
@@ -1,5 +1,12 @@
 package com.example.demo.config;
 
-//@Configuration
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableRedisHttpSession
 public class WebAppConfig {
+//    @Bean
+//    public LettuceConnectionFactory connectionFactory() {
+//        return new LettuceConnectionFactory(); // <2>
+//    }
 }
