99942a727a104d9866b734bf791d67c69c7f3b5a
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-07-25 13:31:50 +0900

    Redisをつかうようにすると同期されない

--- src/main/java/com/example/demo/config/WebAppConfig.java
+++ src/main/java/com/example/demo/config/WebAppConfig.java
@@ -1,12 +1,15 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-//@EnableRedisHttpSession
+@EnableRedisHttpSession
 public class WebAppConfig {
-//    @Bean
-//    public LettuceConnectionFactory connectionFactory() {
-//        return new LettuceConnectionFactory(); // <2>
-//    }
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
 }
